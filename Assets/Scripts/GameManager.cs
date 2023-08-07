c7e2f9cc7373a19cf15ae34b7e0e4f18fbd32d63
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-08-07 16:15:57 -0300

    Singleton

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //patrón singleton
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null) {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(this);
+    }
+}
